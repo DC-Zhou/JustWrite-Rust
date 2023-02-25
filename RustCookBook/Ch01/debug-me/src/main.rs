9509936560f4306ed5c1e10e77448d160538e6ae
zhouzhou <1197236910@qq.com>
2023-02-25 14:46:18 +0800

    add Ch01-Debugging-Rust

---
+++ RustCookBook/Ch01/debug-me/src/main.rs
@@ -0,0 +1,20 @@
+struct MyStruct{
+    num0:usize,
+    num1:f32,
+    num2:f32,
+}
+
+struct Point{
+
+}
+
+fn main() {
+    println!("Hello, How to Debug rust!");
+
+    let a = 42;
+    let b = vec![0,0,0,100];
+    let c = [1,2,3,4,5];
+    let d = 0x5ff;
+    let e  = MyStruct{num0:100, num1: 0.32,  num2:0.45};
+
+}
